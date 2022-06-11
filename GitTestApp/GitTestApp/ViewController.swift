ec8f42d63e20ad39c66d5653d264a6327d9f1adf
LeonHelileo <107199133+LeonHelileo@users.noreply.github.com>
2022-06-11 12:54:09 +0200

    Add Prj

---
+++ GitTestApp/GitTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTestApp
+//
+//  Created by Leonid Gneiting on 11/06/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
