c0a5a6a463ffc0d404d2b1dad5215eb5aac8db13
LeonHelileo <107199133+LeonHelileo@users.noreply.github.com>
2022-06-11 13:57:48 +0200

    added alert

--- GitTestApp/GitTestApp/ViewController.swift
+++ GitTestApp/GitTestApp/ViewController.swift
@@ -15,5 +15,20 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func showAlert() {
+        let alert = UIAlertController(title: "PhotoAccess", message: "Do you allow to use your photos?", preferredStyle: .alert)
+        
+        let okAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        
+        alert.addAction(okAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func showActionSheet() {
+    }
 }
 
